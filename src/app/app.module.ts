9b4d140f1e6a46ba2b57023337efd42910972642
foxsir <838394225@qq.com>
2020-10-09 15:49:15 +0800

    完善资料

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,22 +11,25 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatListModule} from '@angular/material/list';
+import {MatExpansionModule} from '@angular/material/expansion';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FlexLayoutModule,
-    MatButtonModule,
-    MatCardModule,
-    MatIconModule,
-    MatToolbarModule,
-    MatDividerModule,
-    MatListModule
-  ],
+    imports: [
+      BrowserModule,
+      BrowserAnimationsModule,
+      AppRoutingModule,
+      FlexLayoutModule,
+      MatButtonModule,
+      MatCardModule,
+      MatIconModule,
+      MatToolbarModule,
+      MatDividerModule,
+      MatListModule,
+      MatExpansionModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
